c7b9bfe6f24fdbdd1ca530965c9cb52b293b2942
Pradhyumna51 <pradhyumdesai789@gmail.com>
2026-04-28 16:45:41 +0530

    your changes

---
+++ run_server.bat
@@ -0,0 +1,15 @@
+@echo off
+echo =========================================
+echo    Starting CTI Canteen Server...
+echo =========================================
+cd frontend
+
+:: Check if node_modules exists, if not, prompt or install
+if not exist "node_modules\" (
+    echo Installing dependencies first...
+    npm install
+)
+
+echo Starting development server...
+npm run dev
+pause
